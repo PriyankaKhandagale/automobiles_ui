65f17d7b3c2f6a80a57b78af0256c9272ba91e10
Priyanka <priyankabhoi95@gmail.com>
2023-09-03 14:28:44 +0530

    all manager files added

---
+++ src/components/manager/components/ManagerDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManagerDashboard = () => {
+    return (
+        <div>
+            Manager Dashboard Component
+        </div>
+    );
+}
+
+export default ManagerDashboard;
